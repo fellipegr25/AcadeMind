498d1176ef38bf48277875f911deb6c7771840b7
Fellipe Geiger <fellipe.geiger@gmail.com>
2020-09-15 14:56:03 -0300

    First Commit

---
+++ JavaScript Course/basics-assignment-1-start/assignment.js
@@ -0,0 +1,12 @@
+const userInput = 25;
+let result;
+
+result = 18 + userInput;
+result = result - 10;
+result = result * 2;
+result = result / 4;
+result = result % 5;
+
+
+console.log(userInput);
+console.log(result);
